4d01ee0ea0d8d89a5bfb3222be7e46e1f7a7b044
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-12-23 02:26:36 -0800

    title, wip asset path names

---
+++ scripts/assetFileNames.js
@@ -0,0 +1,32 @@
+const brownImgNames = {
+  0: 'brownie1.jpg',
+  1: 'brownie2.jpg',
+  2: 'brownie3.jpg',
+  3: 'brownie4.jpg',
+  4: 'brownie5.jpg',
+  5: 'brownie6.jpg',
+  6: 'brownie7.jpg',
+  7: 'brownie8.jpg'
+};
+
+const brownVidNames = {
+  
+}
+
+const leoImgNames = {
+  0: 'leo1.jpg'
+}
+
+const luckyImgNames = {
+  0: 'lucky1.jpg',
+  1: 'lucky1.jpg',
+  2: 'lucky1.jpg',
+  3: 'lucky1.jpg',
+  4: 'lucky1.jpg',
+  5: 'lucky1.jpg',
+  6: 'lucky1.jpg',
+  7: 'lucky1.jpg',
+  8: 'lucky1.jpg',
+  9: 'lucky1.jpg',
+  10: 'lucky1.jpg',
+}
