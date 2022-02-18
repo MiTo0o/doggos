b305af1c76267ae7e1a1382e20cae3c97411883a
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-02-18 04:36:28 -0800

    big progress for homepage

---
+++ src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Home.scss';
+
+function Home() {
+  return(
+  <div class="home-page">
+    <div claa="center-container">
+      <h1 class="title">Doggos</h1>
+      <p class="text">Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet, consectetur</p>
+      <div className="home-button-container">
+        <button className="about-button" type="button">About</button>
+        <button type="button">Galleries</button>
+      </div>
+
+    </div>
+    <footer class="home-footer">
+      sheeesh
+    </footer>
+  </div>
+  );
+}
+
+export default Home;
