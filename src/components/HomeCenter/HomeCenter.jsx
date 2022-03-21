0813c30fa34338c364001a9a55519db56bb2f2a2
MiTo0o <MiTo0o@users.noreply.github.com>
2022-03-21 00:46:44 +0000

    Prettified Code!

--- src/components/HomeCenter/HomeCenter.jsx
+++ src/components/HomeCenter/HomeCenter.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './HomeCenter.scss';
+import React from "react";
+import "./HomeCenter.scss";
 import { Link } from "react-router-dom";
 
 function HomeCenter() {
-  return(
+  return (
     <div className="center-container">
       <h1 className="title">Doggos</h1>
       <p className="text">Check out deez doggos</p>
       <div className="home-button-container">
-        <button className="about-button" type="button">About</button>
-        <Link
-          to="/gallery"
-        >
+        <button className="about-button" type="button">
+          About
+        </button>
+        <Link to="/gallery">
           <button type="button">Galleries</button>
         </Link>
       </div>
     </div>
   );
-};
+}
 
 export default HomeCenter;
