a4f24d4d770fbc055bb88c65a92dfcd23ae1a8bf
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-03-23 11:16:31 -0700

    revamp top nav bar and add nested menu

---
+++ src/components/NestedMenu/NestedMenu.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import Button from "@mui/material/Button";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Menu } from "@mui/material";
+import { NestedMenuItem } from "mui-nested-menu";
+import { MenuItem } from "@mui/material";
+import { Link } from "react-router-dom";
+import { IconMenuItem } from "mui-nested-menu";
+import PetsIcon from '@mui/icons-material/Pets';
+import HomeIcon from '@mui/icons-material/Home';
+import PhotoLibraryIcon from '@mui/icons-material/PhotoLibrary';
+function NestedMenu() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+  return (
+    <div>
+      <Button size="large" onClick={handleClick} endIcon={<MenuIcon />} color="inherit">
+        Menu
+      </Button>
+      <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+        <IconMenuItem
+          onClick={handleClose}
+          leftIcon={<HomeIcon />}
+          label={'Home'}
+        />
+        <NestedMenuItem
+          leftIcon={<PhotoLibraryIcon />}
+          label={'Galleries'}
+          parentMenuOpen={open}
+        >
+          <Link
+            to='/gallery/Leo'
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            <IconMenuItem
+              onClick={handleClose}
+              leftIcon={<PetsIcon />}
+              label={'Leo'}
+            />
+          </Link>
+          <IconMenuItem
+            onClick={handleClose}
+            leftIcon={<PetsIcon />}
+            label={'Lucky'}
+          />
+          <IconMenuItem
+            onClick={handleClose}
+            leftIcon={<PetsIcon />}
+            label={'Brownie'}
+          />
+        </NestedMenuItem>
+      </Menu>
+    </div>
+  );
+}
+export default NestedMenu;
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -4,24 +4,18 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import HomeIcon from '@mui/icons-material/Home';
+
 
 import pawPrintLogo from "../../assets/logos/paw-print.svg";
+import NestedMenu from "../NestedMenu/NestedMenu";
 
 function TopNav() {
   return (
     <div sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            sx={{ mr: 5 }}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
+          <NestedMenu />
           <Typography
             variant="h6"
             sx={{
@@ -31,7 +25,9 @@ function TopNav() {
           >
             <img src={pawPrintLogo} className="App-logo" alt="paw-print-logo" />
           </Typography>
-          <Button color="inherit">Home</Button>
+          <Button size="large" endIcon={<HomeIcon />} color="inherit">
+            Home
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
