2224310439f81471e2fba1609d9a0be66ab5e20c
MiTo0o <MiTo0o@users.noreply.github.com>
2022-03-23 06:08:17 +0000

    Prettified Code!

--- src/assets/Leo.js
+++ src/assets/Leo.js
@@ -1,1 +1,45 @@
-export const Leo = {"title":"Leo","imgList":[{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/1.webp","width":3,"height":4},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/2.webp","width":3,"height":4},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/3.webp","width":3,"height":4},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/4.webp","width":3,"height":4},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/5.webp","width":3,"height":4},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/6.webp","width":3,"height":4},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/7.webp","width":4,"height":3},{"src":"https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/8.webp","width":3,"height":4}]};
+export const Leo = {
+  title: "Leo",
+  imgList: [
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/1.webp",
+      width: 3,
+      height: 4,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/2.webp",
+      width: 3,
+      height: 4,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/3.webp",
+      width: 3,
+      height: 4,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/4.webp",
+      width: 3,
+      height: 4,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/5.webp",
+      width: 3,
+      height: 4,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/6.webp",
+      width: 3,
+      height: 4,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/7.webp",
+      width: 4,
+      height: 3,
+    },
+    {
+      src: "https://raw.githubusercontent.com/MiTo0o/doggos-static/main/Leo/images/8.webp",
+      width: 3,
+      height: 4,
+    },
+  ],
+};
