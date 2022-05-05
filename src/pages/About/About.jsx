3d76b97edcbc140c789a1805809d9a09fe472885
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-05-05 13:27:41 -0700

    update about page

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../components/Footer/Footer.jsx";
+import AboutCenter from "../../components/AboutCenter/AboutCenter.jsx";
 import React from "react";
 import "./About.scss";
 
 function About() {
   return (
     <div className="about-page">
+      <AboutCenter />
       <Footer />
     </div>
   );
