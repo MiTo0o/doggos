f3ea1c8b8cf58d74f17ddcb4318537333e5991cd
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-05-05 13:49:48 -0700

    redirect 404 page to home

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         {/* page not found */}
-        {/* <Route path="*" element={<PageNotFound />} /> */}
+        <Route path="*" element={<Home />} />
 
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
